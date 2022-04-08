481bdbc3cc4836fe84566526561f082c91642a9b
truonghq <huynhquoctruongit@gamail.com>
2022-04-08 17:41:57 +0700

    update code

--- pages/404.js
+++ pages/404.js
@@ -5,11 +5,11 @@ export default function Custom404() {
     <div>
       <img className="w-[45rem] mx-auto" src="/images/not-found.jpg" alt="" />
 
-      <div className="text-center">
+      <div className="text-center mb-4 mt-4">
         <h2>
-          Go to{' '}
+          Go{' '}
           <Link href="/" passHref>
-            <a className="text-blue-600">home page</a>
+            <a className="text-blue-600">Home page</a>
           </Link>
         </h2>
       </div>
